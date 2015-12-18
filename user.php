d6ceed31ab8a3e2872ed59ff1dd7524a8d357692
unknown <k.haurka@gmail.com>
2015-12-18 15:47:41 +0900

    受信PHP

---
+++ user.php
@@ -0,0 +1,18 @@
+<html>
+<head><title>PHP TEST</title></head>
+<body>
+
+<?php
+
+if(isset($_GET['date'])){
+$date = $_GET['date'];
+print("$date<br>\n")
+}
+
+if(isset($_GET['avg_dB'])){
+$avg_dB = $GET['avg_dB'];
+print("$avg_dB<br>\n"):
+}
+?>
+</body>
+</html>
